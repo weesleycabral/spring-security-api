6be51453680db200da1691bfb03275f568bdfb5c
weesleycabral <weesleylima@hotmail.com>
2024-08-12 16:34:40 -0300

    refactor: Refactor UserRegistrationDTO to include conversion methods

--- src/main/java/com/wesley/security/dto/UserRegistrationDTO.java
+++ src/main/java/com/wesley/security/dto/UserRegistrationDTO.java
@@ -1,5 +1,9 @@
 package com.wesley.security.dto;
 
+import java.util.List;
+
+import com.wesley.security.entity.User;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,4 +15,16 @@ public class UserRegistrationDTO {
   private Long id;
   private String email;
   private String password;
+
+  public static User toEntity(UserRegistrationDTO dto) {
+    return new User(dto.getId(), dto.getEmail(), dto.getPassword());
+  }
+
+  public static UserRegistrationDTO toDTO(User user) {
+    return new UserRegistrationDTO(user.getId(), user.getEmail(), null);
+  }
+
+  public static List<UserRegistrationDTO> toDTOs(List<User> users) {
+    return users.stream().map(UserRegistrationDTO::toDTO).toList();
+  }
 }
