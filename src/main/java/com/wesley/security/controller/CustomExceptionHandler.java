2b2e0583a5e6b8880db16604bf4527e282386027
weesleycabral <weesleylima@hotmail.com>
2024-08-13 09:43:16 -0300

    feat: Add ControllerAdvice annotation to CustomExceptionHandler

--- src/main/java/com/wesley/security/controller/CustomExceptionHandler.java
+++ src/main/java/com/wesley/security/controller/CustomExceptionHandler.java
@@ -1,12 +1,14 @@
 package com.wesley.security.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.wesley.security.exception.ErrorMessage;
 import com.wesley.security.exception.UserNotFoundException;
 
+@ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
   @ExceptionHandler({ UserNotFoundException.class })
   public ResponseEntity<ErrorMessage> handleUserNotFound(UserNotFoundException exception) {
