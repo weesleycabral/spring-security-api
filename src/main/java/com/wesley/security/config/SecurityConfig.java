bdda6d29b0a103f891e13ec99049e001a0aca01f
weesleycabral <weesleylima@hotmail.com>
2024-08-15 16:54:27 -0300

    chore: Add SecurityFilter to SecurityConfig and create CorsConfig

--- src/main/java/com/wesley/security/config/SecurityConfig.java
+++ src/main/java/com/wesley/security/config/SecurityConfig.java
@@ -1,27 +1,50 @@
 package com.wesley.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.wesley.security.infra.SecurityFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+  @Autowired
+  SecurityFilter securityFilter;
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    return http
+    http
         .csrf(csrf -> csrf.disable())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests((auth) -> auth.anyRequest().permitAll())
-        .build();
+        // .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+        // .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+        // .requestMatchers(HttpMethod.POST, "/users").permitAll()
+        // .anyRequest().authenticated())
+        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+    return http.build();
   }
 
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+      throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
   }
 }
