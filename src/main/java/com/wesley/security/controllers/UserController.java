8219e2396f88090d09af7a932d64f4660b2e249c
weesleycabral <weesleylima@hotmail.com>
2024-08-15 17:00:26 -0300

    feat: Add UserController with GET endpoints for retrieving users

---
+++ src/main/java/com/wesley/security/controllers/UserController.java
@@ -0,0 +1,45 @@
+package com.wesley.security.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wesley.security.entity.User;
+import com.wesley.security.exception.UserNotFoundException;
+import com.wesley.security.services.UserService;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+  @Autowired
+  private UserService userService;
+
+  @GetMapping
+  @ResponseStatus(value = HttpStatus.OK)
+  public List<User> getAll() {
+    return userService.getAllUsers();
+  }
+
+  @GetMapping("/id/{id}")
+  @ResponseStatus(value = HttpStatus.OK)
+  public User getById(@PathVariable("id") Long id) throws UserNotFoundException {
+    return userService.getUserById(id);
+  }
+
+  @GetMapping("/email/{email}")
+  @ResponseStatus(value = HttpStatus.OK)
+  public User getByEmail(@Valid @Email @PathVariable("email") String email)
+      throws UserNotFoundException {
+    return userService.getUserByEmail(email);
+  }
+}
