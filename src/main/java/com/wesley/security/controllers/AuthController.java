a6b54558602bffc0cc02fe5f2829d6f5359526dd
weesleycabral <weesleylima@hotmail.com>
2024-08-15 16:58:11 -0300

    feat: add return email and token when sign in and sign up

---
+++ src/main/java/com/wesley/security/controllers/AuthController.java
@@ -0,0 +1,35 @@
+package com.wesley.security.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wesley.security.dto.UserLoginRequestDTO;
+import com.wesley.security.dto.UserRegisterRequestDTO;
+import com.wesley.security.dto.UserResponseDTO;
+import com.wesley.security.exception.InvalidCredentialsException;
+import com.wesley.security.services.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+  @Autowired
+  private UserService userService;
+
+  @PostMapping("/register")
+  public ResponseEntity<UserResponseDTO> register(@Valid @RequestBody UserRegisterRequestDTO userDTO) {
+    return userService.register(userDTO);
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<UserResponseDTO> login(@RequestBody UserLoginRequestDTO userDTO)
+      throws InvalidCredentialsException {
+    return userService.login(userDTO);
+  }
+}
