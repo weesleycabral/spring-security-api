d03f8ad378af2f359d23945a438b9245576ba442
weesleycabral <weesleylima@hotmail.com>
2024-08-15 07:53:39 -0300

    feat: Load environment variables from .env file

--- src/main/java/com/wesley/security/SpringSecurityApplication.java
+++ src/main/java/com/wesley/security/SpringSecurityApplication.java
@@ -3,10 +3,14 @@ package com.wesley.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
 
